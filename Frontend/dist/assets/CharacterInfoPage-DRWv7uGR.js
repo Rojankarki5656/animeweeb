5300228110c604edee2e56d6b1a256de87377fd8
Rojan <www.rojan2020@gmail.com>
2026-05-27 13:16:30 +0545

    added dist

---
+++ Frontend/dist/assets/CharacterInfoPage-DRWv7uGR.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DyAhL6hT.js";const r=()=>e.jsx("div",{className:"h-screen flex justify-center items-center",children:e.jsx("h1",{children:"not implemented"})});export{r as default};
